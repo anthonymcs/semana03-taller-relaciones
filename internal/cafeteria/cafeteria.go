45f81ca2e5fac8aae519022958bf57866170b6a8
Anthony Macias <anthoxbv@gmail.com>
2026-04-27 10:44:30 -0500

    declaración de entidades

--- internal/cafeteria/cafeteria.go
+++ internal/cafeteria/cafeteria.go
@@ -11,4 +11,28 @@ var (
 	ErrSaldoInsuficiente    = errors.New("saldo insuficiente")
 )
 
+// ===================== ENTIDADES =====================
 
+type Cliente struct {
+	ID      int
+	Nombre  string
+	Carrera string
+	Saldo   float64
+}
+
+type Producto struct {
+	ID        int
+	Nombre    string
+	Precio    float64
+	Stock     int
+	Categoria string
+}
+
+type Pedido struct {
+	ID       int
+	Cliente  Cliente
+	Producto Producto
+	Cantidad int
+	Total    float64
+	Fecha    string
+}
