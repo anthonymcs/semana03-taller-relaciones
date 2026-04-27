15790e7dcfad5171de4ee1c80345d0b9adf9becc
Anthony Macias <anthoxbv@gmail.com>
2026-04-27 10:42:34 -0500

    declaración de errores

--- internal/cafeteria/cafeteria.go
+++ internal/cafeteria/cafeteria.go
@@ -1,1 +1,14 @@
 package cafeteria
+
+import "errors"
+
+// ===================== ERRORES =====================
+
+var (
+	ErrClienteNoEncontrado  = errors.New("cliente no encontrado")
+	ErrProductoNoEncontrado = errors.New("producto no encontrado")
+	ErrStockInsuficiente    = errors.New("stock insuficiente")
+	ErrSaldoInsuficiente    = errors.New("saldo insuficiente")
+)
+
+
